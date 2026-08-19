a19380d9a9ca8d467a706979263c4543491b7866
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2026-08-19 12:41:59 +0700

    fix deployment

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'sieksa-frontend',
       script: './.output/server/index.mjs',
       env_production: {
-        NUXT_PUBLIC_API_BASE_URL: 'https://api-sieksa.sdnsukapura01dev.my.id',
+        NUXT_PUBLIC_API_BASE_URL: 'https://api-pintu.sdnsukapura01.sch.id',
         PORT: 4001,
         NODE_ENV: 'production'
       }
