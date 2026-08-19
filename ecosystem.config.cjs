94a4f84e3c1bd0114651f08a3a5b9b88b9ca22b1
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2026-08-19 12:08:15 +0700

    update ci cd

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'sieksa-frontend',
-      port: 4001,
-      exec_mode: 'cluster',
-      instances: 'max',
       script: './.output/server/index.mjs',
       env: {
+        NUXT_PUBLIC_API_BASE_URL: 'https://api-sieksa.sdnsukapura01.sch.id',
+        NUXT_PORT: 4001,
         NODE_ENV: 'production'
       }
     }
